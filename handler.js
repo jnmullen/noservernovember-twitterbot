fa4181016d12c6e14f28774814b28d16820ccdae
James Mullen <james.mullen@bjss.com>
2018-11-07 16:58:52 +0000

    Add first attempt at call to twitter

--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 const request = require('graphql-request').request;
+const twitter = require('twitter');
 
 const endpoint = 'https://icanhazdadjoke.com/graphql'
 
@@ -8,6 +9,12 @@ const query = `{
   }
 }`
 
+const client = new Twitter({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 async function getDadJoke() {
 	const data = await request(endpoint, query);
@@ -16,10 +23,15 @@ async function getDadJoke() {
 }
 
 async function sendTweet(joke) {
-
+	client.post('statuses/update', {status: joke}, function(error,tweet,response) {
+		if(error) throw error;
+		console.log(tweet);
+		console.log(response);
+	});
 }
 
 exports.twitterbot = async (event) => {
-    return await getDadJoke() ;
+    const joke = await getDadJoke() ;
+    return sendTweet(joke);
 };
 
